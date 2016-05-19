33c789cd6cdc092727bc4c01b8adf9142b59f146
ItaiBibi <itaibibi@gmail.com>
2016-05-19 15:33:52 +0300

    some changes - calc_score

--- matlabFiles/calc_score.m
+++ matlabFiles/calc_score.m
@@ -1,12 +1,12 @@
-function [Score] = calc_score(lambda, Option, NumOfCars, sigma, beta, alfa)
+function [Score] = calc_score(lambda, sigma, beta, alfa, Option, NumOfCars)
 % lambda is a double array of size 4*4 each lambda_ij is the matching scalar
 % for the cars traffic light that is comming from lane i*2-1 to lane j*2
 % Option is a boolean array of size 4*4 each index - i,j reffering to
 % a traffic light that is comming from lane i*2-1 to lane j*2. if the index
 % is true: this traffic light is supposed to be green in this option.
 % false: red.
-% NumOfCars a 4*4 array of vectors of size 2*1. In each vector; the first index
-% contains the sum of waiting time, the second contains #cars waiting.  
+% NumOfCars a 4*4 array of vectors. Each vector is of size #cars in i,j.
+% Each slot in the vector contains a waiting time of car k in index i,j.  
 % sigma, beta and alfa are for future functions
 Score = lambda()
 
