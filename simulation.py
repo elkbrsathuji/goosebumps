4b62ef6ee4b1a8da54c37f368e578377c849a605
zahi200 <ajamisz@gmail.com>
2016-05-19 16:44:22 +0300

    simulation zahi
    nothing :)

---
+++ simulation.py
@@ -0,0 +1,16 @@
+def simulate(tick, num_iterations):
+    junction = base_junction();
+    junction.add_lane();
+    t = 0;
+
+
+    while t < num_iterations:
+        
+
+
+
+        t = t+1;
+
+
+if __name__ == "__main__":
+    main(sys.argv)
