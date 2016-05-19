ae14fa879766799bb1b0d257949636f882d76a65
Amit M <mangate@gmail.com>
2016-05-19 16:16:25 +0300

    Added base generator

--- base_generator.py
+++ base_generator.py
@@ -1,4 +1,21 @@
 """
-This generator genrates cars
+This generator generates cars
+Its generates with uniform dictibution
 """
+from base_car import base_car
+import numpy as np
+
+
 class base_generator(object):
+    def __init__(self,prob=0):
+        self._prob = prob
+
+    def generate_car(self,time):
+        x = np.random
+        cars_list = []
+        if x>=self._prob:
+            car = base_car(time)
+            cars_list.append(car)
+        return cars_list
+
+
