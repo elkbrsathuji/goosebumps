5a75a190ae2625904e0e16c610ac4410c74e2211
Amit M <mangate@gmail.com>
2016-05-19 19:50:39 +0300

    Changed stats for lane

--- base_lane.py
+++ base_lane.py
@@ -61,6 +61,6 @@ class base_lane(object):
 		return car_out
 
 	def export(self,time):
-		cars_list = [(i,i.get_time_in_j(time)) for i in self._cars]
+		cars_list = [(i.get_time_in_j(time)) for i in self._cars]
 		return cars_list
 
