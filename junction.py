e919c472407e0adf02cc0ee0b6a446e7899ede24
Amit M <mangate@gmail.com>
2016-05-19 23:21:02 +0300

    added statistics to junction

--- junction.py
+++ junction.py
@@ -16,6 +16,8 @@ class junction(object):
                     self._lanes[i][j] = base_lane(0.4)
                     self._indices.append([i,j])
         print self._indices
+
+        self._timeInJunc= []
     """
     At each tick we get the lights and we call all lanes
     to generate new cars and release cars
@@ -30,20 +32,49 @@ class junction(object):
             j=element[1]
             if t_lights[i][j]==1:
                 cars_out = self._lanes[i][j].tick(time,True)
+
                 if cars_out:
                     out_car_lanes[i][j]=1
+                    self.update_junction_statistics(cars_out.get_time_in_j()) # Erez
+
             else:
                 self._lanes[i][j].tick(time)
         return out_car_lanes
 
+    # Erez:
+    def update_junction_statistics(self,timeInJunc):
+        self._timeInJunc.append(timeInJunc)
+
+
+    def getStatistics(self):
+        meanTime=0
+        maxTime=0
+        amount=0
+        for timeInJunc in self._timeInJunc:
+            meanTime= timeInJunc+meanTime
+            amount+=1
+            if maxTime<timeInJunc:
+                maxTime=timeInJunc
+
+        variance=0
+        for timeInJunc in self._timeInJunc:
+            variance=(meanTime-timeInJunc)*(meanTime-timeInJunc)/amount
+
+        return meanTime,variance,maxTime
+
+
+
+
+
     def get_stats(self,time):
-         stats = [[None for _ in range(4)] for _ in range(4)]
-         for element in self._indices:
+        stats = [[None for _ in range(4)] for _ in range(4)]
+        for element in self._indices:
             i=element[0]
             j=element[1]
             stats[i][j]=self._get_stats_from_lane(i,j,time)
-            print stats[i][j]
+        #print stats[i][j]
             #print stats[i][j][1:4]
+        return stats
 
     """
     This function returns 4 elemnts which are a list of cars and their current waiting time
@@ -53,7 +84,7 @@ class junction(object):
     """
     def _get_stats_from_lane(self,i,j,time):
         cars = self._lanes[i][j].export(time)
-        print "cars are",cars
+        #print "cars are",cars
         #avg = self._lanes[i][j].get_avg()
         #sum = self._lanes[i][j].get_sum()
         #num_of_cars = self._lanes[i][j].num_cars()
