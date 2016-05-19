536eb67895cc381a5c4864701ff03fc5277e407b
Amit M <mangate@gmail.com>
2016-05-19 13:53:41 +0300

    Base car No direction

--- base_car.py
+++ base_car.py
@@ -1,4 +1,23 @@
 class base_car(object):
-    def _init_(self):
-        pass
+    def _init_(self,time):
+        """
+        Basic car object
+        :param time:
+        :return:
+        """
+        self._time_in = time
+        self._time_out = -1
+
+    def get_time_in(self):
+        return  self._time_in
+
+    def get_time_out(self):
+        return self._time_out
+
+    def get_time_in_j(self,time):
+        return (time-self._time_in)
+
+    def set_time_out(self,time):
+        self._time_out = time
+
     
