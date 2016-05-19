c65870238800016af714898f6257966b27a3e59f
elkbrsathuji <elkana.baris@mail.huji.ac.il>
2016-05-19 19:04:39 +0300

    matalabaa

--- matlab2python.py
+++ matlab2python.py
@@ -1,2 +1,10 @@
-from matlab import engine
-print "hi"
+import matlab.engine
+import time
+
+st = time.time()
+
+eng = matlab.engine.start_matlab()
+tf = eng.isprime(37)
+end = time.time()
+
+print "tf", end-st
