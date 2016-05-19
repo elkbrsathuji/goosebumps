fa40dfd40f9dd45fd68ed77623a897a587dcaab3
Amit M <mangate@gmail.com>
2016-05-19 17:08:01 +0300

    junction initial commit
    almost finalized

--- junction.py
+++ junction.py
@@ -7,10 +7,28 @@ class junction(object):
 
     def __init__(self,lanes_array):
         #initialize lanes
-        lanes = [[None for _ in range(4)] for _ in range(4)]
+        self._lanes = [[None for _ in range(4)] for _ in range(4)]
+        #indices are saving all the indixes which have valid lanes
+        self._indices = []
         for i in range(4):
             for j in range(4):
                 if lanes_array[i][j]!=None:
-                    lanes[i][j] = base_lane()
+                    self._lanes[i][j] = base_lane()
+                    self._indices.append([i,j])
+
+    """
+    At each tick we get the lights and we call all lanes
+    to generate new cars and release cars
+    inputs: time - current time
+    t_lights: current status of traffic lights
+    """
+    def tick(self,time,t_lights):
+        for [element] in self._indices:
+            i=element[0]
+            j=element[1]
+            if t_lights[i][j]==1:
+                self._lanes[i][j].tick(time,True)
+            else:
+                self._lanes[i][j].tick(time)
 
     
