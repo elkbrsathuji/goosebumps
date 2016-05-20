cc45198215d96e598c377ce17f65ca32a1706874
ItaiBibi <itaibibi@gmail.com>
2016-05-20 05:12:27 +0300

    dfsfhsfhfbhbfhfhfgdgdg death!!!!!!!

--- yahav_main.m
+++ yahav_main.m
@@ -1,6 +1,7 @@
 function [timeToNext, option] = yahav_main(numOfCars, Traffic_lights)
 
     save('numOfCars.mat', 'numOfCars');
+    save('Traffic_lights.mat','Traffic_lights')
     my_numOfCars = cell(4,4);
     for i = 1:4
         my_numOfCars(:,i) = numOfCars{1,i};
@@ -25,37 +26,37 @@ function [timeToNext, option] = yahav_main(numOfCars, Traffic_lights)
 % Traffic_lights_r = ~options*25;
 % I'm assuming that everytime a car tern right people can walk! Hence there 
 % are only 13 optimal options.
-% for k = 1:size(Traffic_lights_r,3)
-    car_poly = zeros(1,size(options,3));
-    r_expy = zeros(1,size(options,3));
-    g_poly = zeros(1,size(options,3));
-    % going over all options and choosing the maximal score
-    final_score = -40000*ones(1,4);
-    for j = 1:size(options,3)
-        [car_poly(j), r_expy(j), g_poly(j)] =...
-            calc_score(options(:,:,j), my_numOfCars, Traffic_lights_r,...
-            Traffic_lights_g);
-        if (car_expy(j) > final_score(3))
-            final_score(3) = car_expy(j);
-            final_score(4) = j;
-        end
-        end
+car_poly = zeros(1,size(options,3));
+r_expy = zeros(1,size(options,3));
+g_poly = zeros(1,size(options,3));
+total = zeros(1,size(options,3));
+% going over all options and choosing the maximal score
+final_score = -40000*ones(1,2);
+for j = 1:size(options,3)
+    [car_poly(j), r_expy(j), g_poly(j)] =...
+        calc_score(options(:,:,j), my_numOfCars, Traffic_lights_r,...
+        Traffic_lights_g);
+    total(j) = car_poly(j) + r_expy(j) + g_poly(j);
+    if (total(j) > final_score(1))
+        final_score(1) = total(j);
+        final_score(2) = j;
     end
-    % when testing I want to see the score for each call
-    figure; plot(1:13, car_expy)
-    load('time');
-    title(strcat('exp car score of each option for time: ', int2str(time)))
-    figure; plot(1:13, car_poly)
-    title(strcat('poly car score of each option for time: ', int2str(time)))
-    figure; plot(1:13, r_poly)
-    title(strcat('poly red lights score of each option for time: ', int2str(time)))
-    figure; plot(1:13, r_expy)
-    title(strcat('expy red lights score of each option for time: ', int2str(time)))
-    figure; plot(1:13, g_poly)
-    title(strcat('poly green lights score of each option for time: ', int2str(time)))
-    time = time + 1;
-    save('time.mat','time');
 end
+option = options(:,:,final_score(2));
+% when testing I want to see the score for each call
+load('time');
+figure; plot(1:13, total)
+title(strcat('Total score! of each option for time: ', int2str(time)))
+figure; plot(1:13, car_poly)
+title(strcat('poly car score of each option for time: ', int2str(time)))
+figure; plot(1:13, r_poly)
+title(strcat('poly red lights score of each option for time: ', int2str(time)))
+figure; plot(1:13, r_expy)
+title(strcat('expy red lights score of each option for time: ', int2str(time)))
+figure; plot(1:13, g_poly)
+title(strcat('poly green lights score of each option for time: ', int2str(time)))
+time = time + 1;
+save('time.mat','time');
 
 % creating sub options  
 %     masks = zeros(4,4,16);
